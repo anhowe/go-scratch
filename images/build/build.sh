674e65edf6112c6deb968e08412cc8b33fc8dac7
Ubuntu <azureuser@linuxvm.kzdenzv25m5ehfjlk5im03z3tf.dx.internal.cloudapp.net>
2016-09-09 18:16:56 +0000

    adding builder and prod

--- images/build/build.sh
+++ images/build/build.sh
@@ -2,7 +2,14 @@
 
 export GOPATH=/gopath
 cd $GOPATH
-mkdir -p src/github.com/anhowe/
-cd src/github.com/anhowe/
-git clone https://github.com/anhowe/go-scratch.git
-go build github.com/anhowe/go-scratch
+#mkdir -p src/github.com/anhowe/
+#cd src/github.com/anhowe/
+#git clone https://github.com/anhowe/go-scratch.git
+#go build github.com/anhowe/go-scratch
+if [ ! -d $GOPATH/src/github.com/anhowe ] ; then
+  mkdir -p $GOPATH/src/github.com/anhowe/
+  cd $GOPATH/src/github.com/anhowe/
+  git clone https://github.com/anhowe/go-scratch.git
+fi
+ 
+go install github.com/anhowe/go-scratch
